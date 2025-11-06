9b4021f1937da29943c172bb20e15534e4a937dc
irshadkhan24 <irshakhan012003@gmail.com>
2025-11-06 14:01:56 +0530

    Update userSeed.js

--- userSeed.js
+++ userSeed.js
@@ -1,14 +1,16 @@
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-import User from "./models/User.js";
+import mongoose from "mongoose";
+import User from "./models/User.js"; // Make sure User model exists
 import connectToDatabase from "./db/db.js";
 
 dotenv.config();
 
 const userRegister = async () => {
-  await connectToDatabase();
   try {
+    await connectToDatabase();
+
+    console.log("Checking existing admin...");
     const existingUser = await User.findOne({ email: "admin@gmail.com" });
     if (existingUser) {
       console.log("⚠️ Admin already exists.");
@@ -16,12 +18,14 @@ const userRegister = async () => {
     }
 
     const hashPassword = await bcrypt.hash("admin", 10);
+
     const newUser = new User({
       name: "Admin",
       email: "admin@gmail.com",
       password: hashPassword,
       role: "admin",
     });
+
     await newUser.save();
     console.log("✅ Admin user created successfully.");
   } catch (error) {
